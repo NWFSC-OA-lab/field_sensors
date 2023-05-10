7fea34f98e2c673ed3c673c1c3e8518e15503655
axiongH <122634024+axiongH@users.noreply.github.com>
2023-05-09 20:41:41 -0700

    Update shuckmaster.ino
    -Added pico and salinity
    -got rid of durafet stuff

--- ShuckMaster/shuckmaster.ino
+++ ShuckMaster/shuckmaster.ino
@@ -2,20 +2,16 @@
 #include <RTClib.h>
 #include <EEPROM.h>
 #include "src/FileLogger.h"
-
 #include "src/util/PacketReceiver.h"
 #include "src/util/PacketSender.h"
 #include "src/Packets.h"
-
 #include "src/Durafet.h"
 #include <Ezo_i2c.h>
 #include "src/TimedToggleRelay.h"
+#include "SD.h"
 
-Durafet df;  // Create Durafet as df
+
 Ezo_board EC = Ezo_board(100, "EC");      //create an EC circuit object who's address is 100 and name is "EC"
-
-TimedToggleRelay pump(23/* TODO pick GPIO pin*/, 5000);
-
 RTC_DS3231 rtc;  // Instantiate Real-Time Clock as rtc
 FileLogger logger;
 char dataLabel[16] = "PH";
@@ -23,9 +19,7 @@ char dataLabel[16] = "PH";
 HardwareSerial& hm10 = Serial2;
 
 const uint8_t sensorID = 4;
-
 const uint32_t syncPattern = 0x04030201;
-
 const uint32_t eepromMagicAddress = 0x100;  //Magic value and address used to see if a shuck master has been configured before
 const uint32_t eepromMagicValue = 0xCAFEF00D;
 
@@ -39,37 +33,68 @@ PacketSender sender(sendBuf, syncPattern);  // create Reciever buffer to send da
 bool sending = false;
 bool sdHealthy = false;
 bool rtcHealthy = false;
-bool dfHealthy = false;
+//bool dfHealthy = false;
 
 unsigned long lastByteReadMillis = 0;
 unsigned long lastPHStoredMillis = 0;
 
 struct Config cachedConfig;
 
-//Setting varaibles for the salinity sensor
+//Setting variables for the Pico pH
+const char *picoDelimiter =" ";
+char* command;
+char* channel;
+char* s_value;
+char* R0; //command status
+char* R1; //reserved 
+char* R2; //reserved 
+char* R3; //reserved 
+char* R4; //reserved 
+char* R5; //tempSample
+char* R6; //tempCase
+char* R7; //signalIntensity
+char* R8; //ambientLight
+char* R9; //pressure
+char* R10; //humidity
+char* R11; //resisorTemp
+char* R12; //reserved 
+char* R13; //reserved 
+char* R14; //pH
+//float pH;
+//float temperature;
+
+//Setting variables for the salinity sensor
 char* data;
 String data_string;
 char* token;
 const char *delimiter =","; //Used to parse through the data
-//float con;
 char* con;
 char* tds;
 char* sal;
 char* gravity;
 float salinity;
-float salinity_value;
-// Do we need to initialie a date and time object for the clock?
+float conductivity;
+
+//Initializing constants for the SD card (only used for creating the csv file)
+const int CSpin = 53; //SD card chip select is set to pin 53
+File sensorData;
+String dataString = "";
+String fileName = "";
+// Do we need to initialize a date and time object for the clock?
 
 void setup() {
   Serial.begin(9600);  // Set Baud Rate to 9600
+  Serial1.begin(19200); //For the Pico pH
   Serial2.begin(9600);
   Wire.begin();
-  if (logger.Init(53)) {
+
+  //Checks for the SD card and the RTC
+  if (logger.Init(CSpin)) {
     Serial.println("SD card initialized successfully");
     sdHealthy = true;
   } else {
     Serial.println("Failed to initialize SD card! uh oh.");
-    // while (1) {  }
+    
   }
   
   if (rtc.begin()) {
@@ -79,18 +104,10 @@ void setup() {
     Serial.println("Couldn't find RTC");
     // while (1) delay();
   }
+  //
 
   lastByteReadMillis = millis();
   lastPHStoredMillis = millis();
-
-  if (df.Begin()) {
-    Serial.println("Durafet initialized successfully");
-    dfHealthy = true;
-  } else {
-    Serial.println("Couldn't find Durafet");
-  }
-
-  pump.Begin();
 
   // check eeprom for magic value; if not written, assume first run and initialize persistent config
   // this sets the value, ph period, and the eeprommagicvalue/address
@@ -121,11 +138,10 @@ void setup() {
   Serial.println(cachedConfig.stdVoltage, 4);
 
   // update calibration with stored values
-  // Calibrates using the equation in the github wiki. 
-  df.Calibrate(cachedConfig.stdTemperature, cachedConfig.stdPh, cachedConfig.stdVoltage);
-  
+  EC.send_cmd_with_num("K,",1); //setting the k value to 1
   receiver.Begin();
   sending = false;
+
 }
 
 void printDate(const DateTime& date) {
@@ -142,36 +158,77 @@ void printDate(const DateTime& date) {
   Serial.print(date.second(), DEC);
 }
 
+
 void loop() {
-  df.Tick();
-  pump.Tick();
 
   if (rtcHealthy && sdHealthy && millis() - lastPHStoredMillis > cachedConfig.phPeriod * 1000) {
-    float ph = df.GetPh();
-    float temp = df.GetTemp();
-    salinity_value = salinityRead();
-    Serial.print("Made pH, temperature, and salinity measurements! pH: ");
+    
+    //Checks if the SD card has a file for the current day. If not, then it will create the file with the labels for each data
+    DateTime name = rtc.now();
+    fileName = String(name.month()) + "_" + String(name.day()) + "_" + String(name.year()-2000) + ".csv"; //format should look like month_day_year
+    Serial.println(fileName);
+    if (!SD.exists(fileName)){
+      Serial.println("card initialized.");
+      Serial.println("Now creating file...");
+      sensorData = SD.open(fileName, FILE_WRITE);
+
+      if(sensorData){
+        Serial.println("File is open... now creating titles");
+        String titles = String("time") + "," + String("pH") + "," + String("temperature") + "," + String("salinity") + "," + String("conductivity"); // convert to CSV
+        sensorData.println(titles);
+        Serial.println("Done creating titles");
+        sensorData.close(); // close the file
+      }
+      else{
+        Serial.println("File failed to be created/opened");
+      }
+    }
+
+    pico_read(); 
+    float ph = atof(R14)/1000;
+    float temp = atof(R5)/1000;
+    ezoRead(temp);
+    Serial.print("Made pH, temperature, salinity, and conductivity measurements! pH: ");
     Serial.print(ph, 4);
     Serial.print(",\tTemperature: ");
     Serial.print(temp, 4);
     Serial.println(" C");
     Serial.print(",\tSalinity: ");
-    Serial.print(salinity_value, 4);
+    Serial.print(salinity, 4);
     Serial.println(" PSU(ppt)");
+    Serial.print(",\tConductivity: ");
+    Serial.print(conductivity, 4);
+    Serial.println(" uS");
+
     // log ph
+    //Serial.println("Testing logging");
     logger.LogFloat(rtc.now(), "pH", ph);
 
     // log temperature
     logger.LogFloat(rtc.now(), "tp", temp);
 
-    // log salinity_value
-    logger.LogFloat(rtc.now(), "sa", salinity_value);
+    // log salinity
+    logger.LogFloat(rtc.now(), "sa", salinity);
 
-    // logger.LogFloat(rtc.now(), "pH", rtc.now().second());
+    // log conductivity
+    logger.LogFloat(rtc.now(), "co", conductivity);
+
     lastPHStoredMillis = millis();
+    //Serial.println("finished logging");
+
+    //if sensorData file is open, then log of all the measured data in the AllData.csv file
+    sensorData = SD.open(fileName, FILE_WRITE);
+    DateTime time = rtc.now();
+    String timeStamp = String(time.month()) + "/" + String(time.day()) + "/" + String(time.year()) + "  " + String(time.hour()) +":"+ String(time.minute());
+    dataString = String(timeStamp) + "," + String(ph) + "," + String(temp)+ "," + String(salinity)+ "," + String(conductivity); // convert to CSV
+    Serial.print("Now saving the string: ");
+    Serial.println(dataString);
+    sensorData.println(dataString);
+    sensorData.close(); // close the file
   }
   
   if (sending) {
+    Serial.println("Testing sending");
     const int N = 10; // number of entries per packet, TODO put this somewhere else
     struct FileEntry entries[N];
     
@@ -226,6 +283,7 @@ void loop() {
   }
   
   if (hm10.available()) {
+    //Serial.println("Testing hm10av");
     char readByte = hm10.read();
     lastByteReadMillis = millis();    // save time we read a byte at
     if (receiver.AddByte(readByte)) {
@@ -280,25 +338,41 @@ void loop() {
             Serial.println(configPacket.config.phPeriod);
             cachedConfig.phPeriod = configPacket.config.phPeriod;
           }
-          if (configPacket.configField & (1 << CONFIG_STD_TEMP)) {
-            Serial.print("Calibrating with standard temperature: ");
-            Serial.println(configPacket.config.stdTemperature);
-            cachedConfig.stdTemperature = configPacket.config.stdTemperature;
-          }
+
           if (configPacket.configField & (1 << CONFIG_STD_PH)) {
             Serial.print("Calibrating with standard pH: ");
             Serial.println(configPacket.config.stdPh);
             cachedConfig.stdPh = configPacket.config.stdPh;
           }
-          if (configPacket.configField & (1 << CONFIG_STD_V)) {
-            Serial.print("Calibrating with standard voltage: ");
-            Serial.println(configPacket.config.stdVoltage);
-            cachedConfig.stdVoltage = configPacket.config.stdVoltage;
-          }
-          EEPROM.put(0, cachedConfig);
 
-          // update durafet calibration values
-          df.Calibrate(cachedConfig.stdTemperature, cachedConfig.stdPh, cachedConfig.stdVoltage);
+          if (configPacket.configField & (1 << CONFIG_STD_TEMP)) {
+            Serial.print("Calibrating with standard temperature: ");
+            Serial.println(configPacket.config.stdTemperature);
+            cachedConfig.stdTemperature = configPacket.config.stdTemperature;
+
+          }
+
+          if (configPacket.config.stdLowCon != 0.0){
+          
+            Serial.print("Calibrating with standard low conductivity: ");
+            Serial.println(configPacket.config.stdLowCon);
+            cachedConfig.stdLowCon = configPacket.config.stdLowCon;
+
+            EC.send_cmd_with_num("Cal,low,", cachedConfig.stdLowCon); //sending the command "Cal,low,#" 
+            Serial.print("stdLowCon= ");
+            Serial.println(cachedConfig.stdLowCon);
+          }
+
+          if(configPacket.config.stdHighCon != 0.0){
+            Serial.print("Calibrating with standard high conductivity: ");
+            Serial.println(configPacket.config.stdHighCon);
+            cachedConfig.stdHighCon = configPacket.config.stdHighCon;
+
+            EC.send_cmd_with_num("Cal,high,", cachedConfig.stdHighCon); //sending the command "Cal,high,#"
+          }          
+
+          EEPROM.put(0, cachedConfig);
+          
           break;
         }
         case PACKET_DATA: {
@@ -336,33 +410,34 @@ void loop() {
     }
   } else {
     // nothing available, restart receiver state machine if we've stalled for too long (5 seconds)
+    //Serial.println("testing restart");
     if (millis() - lastByteReadMillis > 5000) {
+      //Serial.println("restart");
       lastByteReadMillis = millis();
       receiver.Begin();
     }
   }
 }
 
-float salinityRead(){
-  
-  EC.send_read_cmd();
-  delay(700); //Need a delay of at least 7ms when we send the "R" command 
+
+void ezoRead(float temp){
+
+  EC.send_read_with_temp_comp(temp); //sending the "rt" command with 't' being the temperature compensation 
+  //EC.send_read_cmd();
+  delay(700); //Need a delay of at least 7ms 
   EC.receive_read_cmd();
   //Serial.println();
   data = EC.get_buffer();
-  data_string = String(data);
+  //data_string = String(data);
   //Serial.print("This is the data: ");
   //Serial.println(data_string);
-
   //Serial.println("Now decomposing the data...");
   data_decompose(data);
   //Serial.println();
-  
+  conductivity = atof(con);
   //Serial.print("Extracting the salinity: ");
   salinity = atof(sal); //converts the value to a float
   //Serial.println(salinity);
-
-  return salinity;
 }
 
 //This function will parse through the char array and extract each data by removing the comma and creating tokens as substrings
@@ -383,3 +458,62 @@ void data_decompose(char* buffer){
   //Serial.print("Gravity:");
   //Serial.println(gravity);
 }
+
+
+//This function will parse through the char array and extract each data by removing the space and creating tokens as substrings
+void pico_read(){
+
+  Serial1.println("MEA 1 3");
+  //delay(1500);
+  String received_string = "";
+  
+  while(Serial1.available() == 0) {
+  }
+
+  received_string = Serial1.readStringUntil('\n')
+  //char received_char = Serial1.read();
+  //Serial.print("received_char:");
+  //Serial.println(received_char);
+  //received_string += received_char;
+
+  Serial.println();
+  Serial.print("This is the string: ");
+  Serial.println(received_string);
+
+  char *tokenP = received_string.c_str();
+  command = strtok(tokenP, picoDelimiter);
+  channel = strtok(NULL,picoDelimiter);
+  s_value = strtok(NULL, picoDelimiter);
+  R0 = strtok(NULL, picoDelimiter);
+  R1 = strtok(NULL, picoDelimiter);
+  R2 = strtok(NULL, picoDelimiter);
+  R3 = strtok(NULL, picoDelimiter);
+  R4 = strtok(NULL, picoDelimiter);
+  R5 = strtok(NULL, picoDelimiter); //temperature
+  R6 = strtok(NULL, picoDelimiter);
+  R7 = strtok(NULL, picoDelimiter);
+  R8 = strtok(NULL, picoDelimiter);
+  R9 = strtok(NULL, picoDelimiter);
+  R10 = strtok(NULL, picoDelimiter);
+  R11 = strtok(NULL, picoDelimiter);
+  R12 = strtok(NULL, picoDelimiter);
+  R13 = strtok(NULL, picoDelimiter);
+  R14 = strtok(NULL, picoDelimiter); //pH
+  Serial.println(R5);
+  Serial.println(R14);
+}
+
+/*
+//This function will calibrate the pico pH. The sensor requires a two-point pH calibration so it takes in the low pH, high pH,
+//and the temperature and salinity of the calibration solution
+void pico_calibrate(float lowPH, float highPH, float temp, float sal){
+  // "CPH C N P T S"
+  String lowCommand = "CPH 1 0 " + String(lowPH) +  " " + String(temp) + " " + String(sal); 
+  Serial1.println(lowCommand);
+  Serial1.println("SVS 1"); //permanently saving configuration in flash memory
+  delay(1000);
+  String highCommand = "CPH 1 0 " + String(highPH) +  " " + String(temp) + " " + String(sal); 
+  Serial1.println(highCommand);
+  Serial1.println("SVS 1");
+}
+*/
