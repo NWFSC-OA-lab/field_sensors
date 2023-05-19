24b55da62695085537738ae28ff276c64c9011fa
axiongH <122634024+axiongH@users.noreply.github.com>
2023-05-18 21:33:13 -0700

    Update PacketType.kt
    Added packets for the different types of errors, and the current reading packet

--- ShuckManager/app/src/main/java/com/noaa/shuckmanager/PacketType.kt
+++ ShuckManager/app/src/main/java/com/noaa/shuckmanager/PacketType.kt
@@ -5,5 +5,9 @@ enum class PacketType(val code: Byte) {
     PING(0x00),
     HEALTH(0x01),
     CONFIG(0x02),
+    RTC_ERROR(0x04),
+    SD_ERROR(0x05),
+    PICO_ERROR(0x06),
+    CURRENT_DATA(0x07),
     DATA(0x08);
 }
